2f5f5736f12e865b53d27e9a322eeda933f3c1ad
Kettner Samuel <kettners21@spstrutnov.cz>
2023-03-27 09:44:34 +0200

    jídl

--- python/src/snake.py
+++ python/src/snake.py
@@ -18,8 +18,62 @@ def init(game):
 def move(game):
     #
     # SEM UMISTUJTE SVUJ KOD
-    print(game ["food"["x"]]):
-        
+    if game["you"]["head"]["x"] + game["board"]["food"]["x"] > game["you"]["head"]["y"] + game["board"]["food"]["y"]:
+        if game["you"]["head"]["x"] < game["board"]["food"]["x"]:
+            if moveLefttPossible == True:
+                result = 'Left'
+            else:
+                if game["you"]["head"]["y"] < game["board"]["food"]["y"]:
+                    if moveUpPossible == True:
+                        result = 'Up'
+                elif game["you"]["head"]["y"] > game["board"]["food"]["y"]:
+                    if moveDownPossible == True:
+                        result = 'Down'
+                else:
+                    pass
+                
+        if game["you"]["head"]["x"] > game["board"]["food"]["x"]:
+            if moveRightPossible == True:
+                result = 'Right'
+            else:
+                if game["you"]["head"]["y"] < game["board"]["food"]["y"]:
+                    if moveUpPossible == True:
+                        result = 'Up'
+                elif game["you"]["head"]["y"] > game["board"]["food"]["y"]:
+                    if moveDownPossible == True:
+                        result = 'Down'
+                else:
+                    pass
+                
+    elif game["you"]["head"]["x"] - game["board"]["food"]["x"] < game["you"]["head"]["y"] - game["board"]["food"]["y"]:
+        if game["you"]["head"]["y"] < game["board"]["food"]["y"]:
+            if moveUpPossible == True:
+                result = 'Up'
+            else:
+                if game["you"]["head"]["x"] > game["board"]["food"]["x"]:
+                    if moveRightPossible == True:
+                        result = 'Right'
+                elif game["you"]["head"]["x"] < game["board"]["food"]["x"]:
+                    if moveLefttPossible == True:
+                        result = 'Left'
+                else:
+                    pass
+                        
+        elif game["you"]["head"]["y"] > game["board"]["food"]["y"]:
+            if moveDownPossible == True:
+                result = 'Down'
+            else:
+                if game["you"]["head"]["x"] > game["board"]["food"]["x"]:
+                    if moveRightPossible == True:
+                        result = 'Right'
+                elif game["you"]["head"]["x"] < game["board"]["food"]["x"]:
+                    if moveLefttPossible == True:
+                        result = 'Left'
+                else:
+                    pass
+     elif game["you"]["head"]["x"] + game["board"]["food"]["x"] == game["you"]["head"]["y"] + game["board"]["food"]["y"]:
+        pass
     #
+    
     # pro ukazku se vraci nahodny smer
     
